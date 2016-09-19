ca7577fefa492249123f96a5ee793dc0c0eedd55
Jean Nunes <jeanrnunes@gmail.com>
2016-09-19 11:01:43 -0300

    Added some parameters
    Now you can chose if you want to add a background overlay and what color
    it would be

--- production/jquery.modal.1.0.js
+++ production/jquery.modal.1.0.js
@@ -1,3 +1,49 @@
-$($.fn.modal = function(method, options = null){
-  
-})(JQuery);
+(function($) {
+
+// Modal
+
+$.fn.modal = function(method, options) {
+
+	var obj = $(this);
+	
+	var defaults = {
+			width : 380,
+			height : 240,
+			top : ($(window).width() - $(obj).width()) / 2,
+			left : ($(window).height() - $(obj).height()) / 2,
+			position: "fixed",
+			background: true,
+			backgroundColor: "RGA(0,0,0,0.8)"
+		};
+
+	var settings = $.extend( {}, defaults, options ),
+		background = $("<div></div>");
+
+	$(background).css({
+		"background-color": settings.backgroundColor,
+		"position": "fixed",
+		"top": "0px",
+		"left": "0px",
+		"right": "0px",
+		"bottom": "0px",
+	});
+
+	$(this)
+		.show()
+		.css({
+			position: settings.position,
+			top: settings.top,
+			left: settings.left,
+			width: settings.width,
+			height: settings.height,
+		})
+		.draggable();
+		if (settings.background) {$(this).before(background);}
+	
+	$(".close-modal", this).click(function(){
+		if (settings.background) {$(background).remove();}
+		$(obj).hide();
+	});
+
+	return this;
+};
