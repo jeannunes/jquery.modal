a7fd5a3b0fa0f7200c2a29a194efb81ae94e0177
Jean Nunes <jeanrnunes@gmail.com>
2016-09-22 16:27:45 -0300

    Update jquery.modal.js

--- production/jquery.modal.js
+++ production/jquery.modal.js
@@ -6,33 +6,37 @@
 	});
 	$.fn.modal = function(method, options = null) {
 		var obj = $(this);
+		var defaults = {
+			width: $(obj).width(),
+			height: $(obj).height(),
+			top: ($(window).width() - $(obj).width()) / 2,
+			left: ($(window).height() - $(obj).height()) / 2,
+			position: "fixed",
+			background: true,
+			backgroundColor: "RGBA(0,0,0,0.8)",
+			backgroundClose: true,
+			close: ".modal-close",
+			beforeOpen: function() {},
+			afterOpen: function() {},
+			beforeClose: function() {},
+			afterClose: function() {}
+		};
+		var settings = $.extend({}, defaults, options);
 		if (method == "open") {
-			var defaults = {
-				width: $(obj).width(),
-				height: $(obj).height(),
-				top: ($(window).width() - $(obj).width()) / 2,
-				left: ($(window).height() - $(obj).height()) / 2,
-				position: "fixed",
-				background: true,
-				backgroundColor: "RGBA(0,0,0,0.8)",
-				backgroundClose: true,
-				close: ".modal-close",
-				beforeOpen: function() {},
-				afterOpen: function() {},
-				beforeClose: function() {},
-				afterClose: function() {}
-			};
-			var settings = $.extend({}, defaults, options),
-				background = $("<div></div>");
-			$(background).addClass('modal-background').css({
-				"background-color": settings.backgroundColor,
-				"position": "fixed",
-				"top": "0px",
-				"left": "0px",
-				"right": "0px",
-				"bottom": "0px",
-				"z-index": 1000
-			});
+			
+			if (settings.background) {
+				var background = $("<div></div>");
+				$(background).addClass('modal-background').css({
+					"background-color": settings.backgroundColor,
+					"position": "fixed",
+					"top": "0px",
+					"left": "0px",
+					"right": "0px",
+					"bottom": "0px",
+					"z-index": 1000
+				});
+			}
+			
 			settings.beforeOpen();
 			$(this).show().css({
 				position: settings.position,
@@ -45,9 +49,11 @@
 				handle: ".modal-title",
 				cursor: "crosshair"
 			});
+			
 			if (settings.background) {
 				$(this).before(background);
 			}
+			
 			settings.afterOpen();
 
 			function close(direct = false) {
